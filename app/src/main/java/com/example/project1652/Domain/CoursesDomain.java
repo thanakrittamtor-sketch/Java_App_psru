c183cfbc0b5ee7fd26d156dda47b86f35bf52cf6
Thanakrit Tamtor <thanakrittamtor@gmail.com>
2026-09-20 17:43:19 +0700

    Add files via upload

---
+++ app/src/main/java/com/example/project1652/Domain/CoursesDomain.java
@@ -0,0 +1,18 @@
+package com.example.project1652.Domain;
+
+public class CoursesDomain {
+    private String title;
+
+    public CoursesDomain(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+}
